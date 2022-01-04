a6e5cacc1501f6711e7fdb55ccda7f66cadfe5eb
MaitaneAbad <maitaneabadconde@gmail.com>
2022-01-04 17:39:21 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import '../styles/App.scss';
+
+//import { Link, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    // <div>
+    //   {/* <Route path='/contacto'></Route> */}
+
+    //   <nav>
+    //     <ul>
+    //       <li>{/* <Link to='/'></Link> */}</li>
+    //       <li>{/* <Link to='/contacto'></Link> */}</li>
+    //     </ul>
+    //   </nav>
+    // </div>
+    <>Pantalla principal para el juego del trivial</>
+  );
+};
+
+export default App;

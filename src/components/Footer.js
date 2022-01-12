e636f3921f60184effadfe9c28fd35a149f23e2e
MaitaneAbad <maitaneabadconde@gmail.com>
2022-01-12 20:02:38 +0100

    test router dom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,49 @@
-import '../styles/components/Footer.scss';
 import { NavLink } from 'react-router-dom';
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <nav>
-                <ul className="footer__menu">
-                    <li className="footer__menu-item">
-                        <NavLink className={(navData) => { return (navData.isActive ? "footer__menu-link active" : "footer__menu-link ") }} to="/">
-                            A jugar
-                        </NavLink>
-                    </li>
-                    <li className="footer__menu-item">
-                        <NavLink className={(navData) => { return (navData.isActive ? "footer__menu-link active" : "footer__menu-link ") }} to="/instructions">
-                            ¿Cómo se juega?
-                        </NavLink>
-                    </li>
-                    <li className="footer__menu-item">
-                        <NavLink className={(navData) => { return (navData.isActive ? "footer__menu-link active" : "footer__menu-link ") }} to="/options">
-                            Más opciones
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
-            <small className="footer__copy">© Adalab</small>
-        </footer>
-    )
-}
+  return (
+    <footer className='footer'>
+      <nav>
+        <ul className='footer__menu'>
+          <li className='footer__menu-item'>
+            <NavLink
+              className={(navData) => {
+                return navData.isActive
+                  ? 'footer__menu-link active'
+                  : 'footer__menu-link ';
+              }}
+              to='/'
+            >
+              A jugar
+            </NavLink>
+          </li>
+          <li className='footer__menu-item'>
+            <NavLink
+              className={(navData) => {
+                return navData.isActive
+                  ? 'footer__menu-link active'
+                  : 'footer__menu-link ';
+              }}
+              to='/instructions'
+            >
+              ¿Cómo se juega?
+            </NavLink>
+          </li>
+          <li className='footer__menu-item'>
+            <NavLink
+              className={(navData) => {
+                return navData.isActive
+                  ? 'footer__menu-link active'
+                  : 'footer__menu-link ';
+              }}
+              to='/options'
+            >
+              Más opciones
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <small className='footer__copy'>© Adalab</small>
+    </footer>
+  );
+};
 export default Footer;

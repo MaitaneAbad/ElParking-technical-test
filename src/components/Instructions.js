84387f66dac6a073f94b79c35f6beaf59d15a304
MaitaneAbad <maitaneabadconde@gmail.com>
2022-01-13 10:09:42 +0100

    add new link to instructions

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,4 +1,19 @@
+import { NavLink } from 'react-router-dom';
+import '../styles/layout/instructions.scss';
 const Instructions = () => {
-  return <>Instruciones</>;
+  return (
+    <div className='instructions'>
+      <NavLink to='/'>
+        <button className='instructions__button'>Volver</button>
+      </NavLink>
+      <section className='instructions__section'>
+        <p className='instructions__section--paragraph'>
+          Bienvenido a Tri-vi-al, este juego consiste en responder 10 preguntas
+          basadas en matemáticas y ver el resultado de las preguntas una vez
+          contestadas.
+        </p>
+      </section>
+    </div>
+  );
 };
 export default Instructions;
